caa87e5f032a39321a64fc446c0c94ed11440a3e
DaanBakker <daanbakker1995@users.noreply.github.com>
2016-12-08 09:37:01 +0100

    Update hardware-catalogus.php

--- hardware-catalogus.php
+++ hardware-catalogus.php
@@ -19,7 +19,7 @@
         <!--- theme --->
         <link href="css/hardware.css" rel="stylesheet">
         <!--- category img CSS -->
-        <link href="css/catagorie.css" rel="stylesheet">
+        <link href="css/categorie.css" rel="stylesheet">
 
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
