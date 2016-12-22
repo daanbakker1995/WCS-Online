c6600d8427366cdec6a172e25c9c89132e6959b0
milanweiss <milanweiss10@gmail.com>
2016-12-22 08:57:01 +0100

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -2,22 +2,44 @@
 include './classes/Database.php';
 $db = new Database();
 
+session_start();
+if (!isset($_SESSION['count'])) {
+    $_SESSION['count'] = 0;
+}
+
 if (isset($_POST["signin"])) {
     $email = $_POST["email"];
     $password = $_POST["password"];
+
 
     $db->query("SELECT * FROM user WHERE user_email=:email");
     $db->bind(':email', $email);
     $row = $db->single();
 
-    if ($row['user_email'] == $email && password_verify($password, $row['user_password'])) {
-        session_start();
-        $_SESSION["ingelogd"] = true;
-        $_SESSION["email"] = $email;
-        header('location: ./index.php');
+    if ($row['user_email'] == $email) {
+
+        if ($row['user_active'] == 0) {
+            echo "Uw account is geblokkeerd.";
+        } else {
+            if ($_SESSION['count'] > 2) {
+                $db->query("UPDATE user SET user_active = 0 WHERE user_email =:email");
+                $db->bind(':email', $email);
+                $db->execute();
+                echo "U heeft te vaak geprobeerd in te loggen,uw account wordt geblokkeerd. ";
+                $_SESSION['count'] = 0;
+            } else {
+                if (password_verify($password, $row['user_password'])) {
+                    $_SESSION["ingelogd"] = true;
+                    $_SESSION["email"] = $email;
+                    header('location: ./index.php');
+                } else {
+                    ++$_SESSION['count'];
+                    echo "Verkeerde gegevens ingevoerd.";
+                }
+            }
+        }
     } else {
-        print ("Emailadres/wachtwoord onjuist.<br>");
-        print ("Probeer opnieuw");
+        echo "Verkeerde gegevens ingevoerd.";
     }
 }
 ?><!DOCTYPE html>
@@ -42,7 +64,8 @@ and open the template in the editor.
                 <p id="profile-name" class="profile-name-card"></p>
                 <form class="form-signin" method="POST" >
                     <span id="reauth-email" class="reauth-email"></span>
-                    <input type="email" id="inputEmail" class="form-control" name="email" placeholder="Email address" required autofocus>
+                    <input type="hidden" name="counter" value="<?php echo $_SESSION['count']; ?>" />
+                    <input type="text" id="inputEmail" class="form-control" name="email" placeholder="Username" required autofocus>
                     <input type="password" id="inputPassword" class="form-control" name="password" placeholder="Password" required>
                     <button class="btn btn-lg btn-primary btn-block btn-signin" type="submit" name = signin>Sign in</button>
                 </form><!-- /form -->
